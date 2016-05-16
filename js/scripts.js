399e09b73d2dc50d82393def2abb2d1fb18740eb
jeaninerees <jeanine.rees@gmail.com>
2016-05-16 02:22:33 -0400

    As of 05-15

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 
-$('.image').modaal({
+$(document).ready(function(){
+$(this).scrollTop(0);
+});
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
+
+
+$('.gallery').modaal({
 		type: 'image'
 });
 
