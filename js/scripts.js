9afcbb5f923c9e4a86b64aa3e1ff07f8810e6a8c
jeaninerees <jeanine.rees@gmail.com>
2016-05-17 12:53:58 -0400

    Pretty much done!

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
 
-$(document).ready(function(){
-$(this).scrollTop(0);
-});
-
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
         $(this).collapse('hide');
